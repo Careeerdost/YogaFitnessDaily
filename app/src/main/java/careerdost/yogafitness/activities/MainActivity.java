be8aa10484e8d87a02c679d93b51ae841cd65bee
Careerdost Enterprise <rohitsha@gmail.com>
2019-04-30 18:48:59 +0530

    Update MainActivity.java

--- app/src/main/java/careerdost/yogafitness/activities/MainActivity.java
+++ app/src/main/java/careerdost/yogafitness/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package careerdost.yogafitness.activities;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -28,11 +27,6 @@ import com.google.android.gms.ads.InterstitialAd;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import careerdost.yogafitness.R;
-import careerdost.yogafitness.menu_activities.Aboutus;
-import careerdost.yogafitness.menu_activities.Feedback;
-import careerdost.yogafitness.menu_activities.MoreApps;
-import careerdost.yogafitness.network_check.CheckNetwork;
-import careerdost.yogafitness.recycler_activities.RecyclerYoga30Day;
 import careerdost.yogafitness.recycler_activities.RecyclerYogaExtras;
 import careerdost.yogafitness.recycler_activities.RecyclerYogaInstruct;
 import careerdost.yogafitness.recycler_activities.RecyclerYogaPrograms;
@@ -41,7 +35,6 @@ import careerdost.yogafitness.recycler_activities.RecyclerYogaTips;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "YogaFitness";
-    private Dialog backpressedDialog;
     private InterstitialAd interstitialAd = null;
 
     @Override
@@ -49,8 +42,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.main);
-
-        backpressedDialog = new Dialog(this);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -64,44 +55,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        // [START retrieve_current_token]
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(task -> {
-                    if (!task.isSuccessful()) {
-                        Log.w(TAG, "getInstanceId failed", task.getException());
-                        return;
-                    }
-
-                    // Get new Instance ID token
-                    String token = task.getResult().getToken();
-
-                    // Log and toast
-                    String msg = getString(R.string.msg_token_fmt, token);
-                    Log.d(TAG, msg);
-                    /*Toast.makeText(main_act.this, msg, Toast.LENGTH_SHORT).show();*/
-                });
-        // [END retrieve_current_token]
-
-        CardView cardExercises = findViewById(R.id.cardExercises);
-        CardView cardDay30 = findViewById(R.id.cardDay30);
-        CardView cardPrograms = findViewById(R.id.cardPrograms);
+        
         CardView cardInstruct = findViewById(R.id.cardInstruct);
         CardView cardTips = findViewById(R.id.cardTips);
         CardView cardExtras = findViewById(R.id.cardReminder);
 
-        cardExercises.setOnClickListener((View v) -> {
-            Intent intent = new Intent(MainActivity.this, Daily_Training.class);
-            startActivity(intent);
-        });
-        cardDay30.setOnClickListener((View v) -> {
-            Intent intent = new Intent(MainActivity.this, RecyclerYoga30Day.class);
-            startActivity(intent);
-        });
-        cardPrograms.setOnClickListener((View v) -> {
-            Intent intent = new Intent(MainActivity.this, RecyclerYogaPrograms.class);
-            startActivity(intent);
-        });
         cardInstruct.setOnClickListener((View v) -> {
             Intent intent = new Intent(MainActivity.this, RecyclerYogaInstruct.class);
             startActivity(intent);
@@ -127,56 +85,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.nav_aboutUs) {
             Intent aboutus = new Intent(this, Aboutus.class);
             startActivity(aboutus);
-        } else if (id == R.id.nav_calendar) {
-            Intent calendar = new Intent(this, CalendarPage.class);
-            startActivity(calendar);
-        } else if (id == R.id.nav_setting) {
-            Intent setting = new Intent(this, SettingPage.class);
-            startActivity(setting);
         } else if (id == R.id.nav_privacypolicy) {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("http://careerdost.in/privacy-policy"));
                 startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
         } else if (id == R.id.nav_pinPage) {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("https://in.pinterest.com/careerdost/"));
                 startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
         } else if (id == R.id.nav_twPage) {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("https://twitter.com/careerdost"));
                 startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
         } else if (id == R.id.nav_fbPage) {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("https://www.facebook.com/careerdost.in"));
                 startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
         } else if (id == R.id.nav_shareApp) {
             Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
             sharingIntent.setType("text/plain");
@@ -187,67 +119,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Have you tried Yoga Fitness?");
             sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
             startActivity(Intent.createChooser(sharingIntent, "Share via"));
-        } else if (id == R.id.nav_moreApps) {
-            Intent moreapps = new Intent(this, MoreApps.class);
-            startActivity(moreapps);
-        } else if (id == R.id.nav_feedback) {
-            Intent feedback = new Intent(this, Feedback.class);
-            startActivity(feedback);
         } else if (id == R.id.nav_update) {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("https://bit.ly/2mm6Lhq"));
                 startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        backpressedDialog.setContentView(R.layout.backpressed_dialog);
-        ImageView closeDialog = backpressedDialog.findViewById(R.id.close_dialog);
-        Button rateus = backpressedDialog.findViewById(R.id.rateus);
-        Button quit = backpressedDialog.findViewById(R.id.quit);
-
-        closeDialog.setOnClickListener((View v) -> {
-            backpressedDialog.dismiss();
-        });
-        rateus.setOnClickListener((View v) -> {
-            if (CheckNetwork.isOnline(MainActivity.this)) //returns true if internet available
-            {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://bit.ly/2mm6Lhq"));
-                startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
-            }
-        });
-        quit.setOnClickListener((View v) -> {
-            if (interstitialAd.isLoaded()) {
-                interstitialAd.show();
-                interstitialAd.setAdListener(new AdListener() {
-                    @Override
-                    public void onAdClosed() {
-                        super.onAdClosed();
-                        finish();
-                    }
-                });
-            } else {
-                finish();
-            }
-        });
-        backpressedDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        backpressedDialog.show();
     }
 }
